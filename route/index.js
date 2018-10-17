068e01ab9fc7c38b2a3330b5e19d711689462354
julianjca <julianjca11@gmail.com>
2018-10-17 13:20:14 +0700

    recommendation nearby

--- route/index.js
+++ route/index.js
@@ -6,6 +6,11 @@ const user = require('./user')
 // router.use('/expense', expense)
 router.use('/zomato',zomato)
 router.use('/user',user)
+const recommendation = require('./RecommendationRoute')
+
+router.use('/expense', expense)
+router.use('/zomato',zomato)
+router.use('/recommendation', recommendation)
 
 router.get('/', function(req, res, next) {
   res.send('<h1>Halo</h1>')
