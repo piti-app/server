dfb33fb3340b8ff0350df64a6b9f74364fa3f2ec
julianjca <julianjca11@gmail.com>
2018-10-17 12:07:24 +0700

    change user schema

--- models/user.js
+++ models/user.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose')
 
-const schema = new mongoose.Schema({ 
-    username: "string",
-    email: "string",
-    avatar: "string",
-    expense: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Expense'
-    }]
+const schema = new mongoose.Schema({
+   name: "string",
+   email: "string",
+   avatar: "string",
+   budget: { type: Number, default: 0 },
+   main_balance: { type: Number, default: 0 },
+   money_spent: { type: Number, default: 0 },
+   expense: [{
+       type: mongoose.Schema.Types.ObjectId,
+       ref: 'Expense'
+   }]
 }, {
-    timestamps: true
+   timestamps: true
 });
 
 
