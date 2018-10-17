5d8ac1a2fec6f7d8f2511cbfdca6c34ea4328351
khodhirob <odirobbani@gmail.com>
2018-10-17 11:30:24 +0700

    user testing

--- test/user.js
+++ test/user.js
@@ -3,8 +3,8 @@ const chaiHttp = require('chai-http')
 chai.use(chaiHttp)
 
 const should = chai.should();
-const User = require('../models/User')
-const Expense = require('../models/Expense')
+const User = require('../models/user')
+const Expense = require('../models/expense')
 const server = require('../app')
 
 describe('user GET', function () {
@@ -13,11 +13,7 @@ describe('user GET', function () {
     const user = new User({
       username: "khodhi",
       email: "khodhirobbani@gmail.com",
-      avatar: "gambar",
-      expense: [{
-          type: mongoose.Schema.Types.ObjectId,
-          ref: 'Expense'
-      }]
+      avatar: "gambar"     
     })
     user.save((err,res) => {
       id_User = res._id
@@ -49,7 +45,7 @@ describe('user GET', function () {
   describe('get user', function () {
     it('it should get user login data ', function (done) {
       chai.request(server)
-      .get('/user')
+      .get('/user/:email')
       .end(function(err, res) {        
         res.body.should.be.an('object').to.have.property('user').with.lengthOf(1).should.be.an('object')
         res.body.user.should.have.property('expense').with.lengthOf(1)
@@ -71,180 +67,180 @@ describe('user GET', function () {
   })
 })
 
-describe('trainer POST', function () {
-  let id_pokemon = ''
-  beforeEach(function (done) {
-    const pokemon = new Pokemon({
-      name: "Charmender",
-      level: 1,
-      type: "fire",
-    })
-    pokemon.save((err,res) => {
-      id_pokemon = res._id
-      done()
-    })
-  })
-  afterEach(function (done) {
-    Trainer.remove({}, function (err) {
-      done()
-    })
-  })
+// describe('trainer POST', function () {
+//   let id_pokemon = ''
+//   beforeEach(function (done) {
+//     const pokemon = new Pokemon({
+//       name: "Charmender",
+//       level: 1,
+//       type: "fire",
+//     })
+//     pokemon.save((err,res) => {
+//       id_pokemon = res._id
+//       done()
+//     })
+//   })
+//   afterEach(function (done) {
+//     Trainer.remove({}, function (err) {
+//       done()
+//     })
+//   })
 
-  afterEach(function (done) {
-    Pokemon.remove({}, function (err) {
-      done()
-    })
-  })
+//   afterEach(function (done) {
+//     Pokemon.remove({}, function (err) {
+//       done()
+//     })
+//   })
 
-  describe('post one user', function () {
-    it('it should post one Trainer data ', function (done) {
-      chai.request(server)
-      .post('/api/user')
-      .send({
-        nickname: "Ash Ketchum",
-        gender: "Male",
-        pokemons: [id_pokemon]
-      })
-      .end(function(err, res) {
-        res.body.should.be.an('object').to.have.property('msg')
-        res.body.should.be.an('object').to.have.property('trainer')
-        res.body.trainer.should.have.property('pokemons').with.lengthOf(1)
-        res.body.trainer.should.have.property('nickname')
-        res.body.trainer.should.have.property('gender')
-        res.body.trainer.should.have.property('createdAt')
-        res.body.trainer.should.have.property('updatedAt')
-        res.body.trainer.should.have.property('__v')
-        res.should.have.status(201)
-        done()
-      })
-    })
-  })
-})
+//   describe('post one user', function () {
+//     it('it should post one Trainer data ', function (done) {
+//       chai.request(server)
+//       .post('/api/user')
+//       .send({
+//         nickname: "Ash Ketchum",
+//         gender: "Male",
+//         pokemons: [id_pokemon]
+//       })
+//       .end(function(err, res) {
+//         res.body.should.be.an('object').to.have.property('msg')
+//         res.body.should.be.an('object').to.have.property('trainer')
+//         res.body.trainer.should.have.property('pokemons').with.lengthOf(1)
+//         res.body.trainer.should.have.property('nickname')
+//         res.body.trainer.should.have.property('gender')
+//         res.body.trainer.should.have.property('createdAt')
+//         res.body.trainer.should.have.property('updatedAt')
+//         res.body.trainer.should.have.property('__v')
+//         res.should.have.status(201)
+//         done()
+//       })
+//     })
+//   })
+// })
 
-describe('trainer UPDATE', function () {
-  let id = ''
-  let id_pokemon = ''
-  beforeEach(function (done) {
-    const pokemon = new Pokemon({
-      name: "Charmender",
-      level: 1,
-      type: "fire",
-    })
-    pokemon.save((err,res) => {
-      id_pokemon = res._id
-      const newTrainer = new Trainer({
-        nickname: "Ash Ketchum",
-        gender: "Male",
-        pokemons: [id_pokemon]
-      })
-      newTrainer.save((err,res) => {
-        id = res._id
-        done()
-      })
-    })
-  })
+// describe('trainer UPDATE', function () {
+//   let id = ''
+//   let id_pokemon = ''
+//   beforeEach(function (done) {
+//     const pokemon = new Pokemon({
+//       name: "Charmender",
+//       level: 1,
+//       type: "fire",
+//     })
+//     pokemon.save((err,res) => {
+//       id_pokemon = res._id
+//       const newTrainer = new Trainer({
+//         nickname: "Ash Ketchum",
+//         gender: "Male",
+//         pokemons: [id_pokemon]
+//       })
+//       newTrainer.save((err,res) => {
+//         id = res._id
+//         done()
+//       })
+//     })
+//   })
 
-  afterEach(function (done) {
-    Trainer.remove({}, function (err) {
-      done()
-    })
-  })
+//   afterEach(function (done) {
+//     Trainer.remove({}, function (err) {
+//       done()
+//     })
+//   })
 
-  afterEach(function (done) {
-    Pokemon.remove({}, function (err) {
-      done()
-    })
-  })
+//   afterEach(function (done) {
+//     Pokemon.remove({}, function (err) {
+//       done()
+//     })
+//   })
 
-  describe('update one Trainer', function () {
-    it('it should update one Trainer data ', function (done) {
-      chai.request(server)
-      .put('/api/user/'+id)
-      .send({
-        nickname: "Brock"
-      })
-      .end(function(err, res) {
-        res.body.should.be.an('object').to.have.property('msg')
-        res.body.should.be.an('object').to.have.property('trainer')
-        res.body.trainer.should.have.property('pokemons').with.lengthOf(1)
-        res.body.trainer.should.have.property('nickname')
-        res.body.trainer.should.have.property('gender')
-        res.body.trainer.should.have.property('createdAt')
-        res.body.trainer.should.have.property('updatedAt')
-        res.body.trainer.should.have.property('__v')
-        res.body.trainer.nickname.should.equal('Brock')
-        res.should.have.status(201)
-        done()
-      })
-    })
-  })
-})
+//   describe('update one Trainer', function () {
+//     it('it should update one Trainer data ', function (done) {
+//       chai.request(server)
+//       .put('/api/user/'+id)
+//       .send({
+//         nickname: "Brock"
+//       })
+//       .end(function(err, res) {
+//         res.body.should.be.an('object').to.have.property('msg')
+//         res.body.should.be.an('object').to.have.property('trainer')
+//         res.body.trainer.should.have.property('pokemons').with.lengthOf(1)
+//         res.body.trainer.should.have.property('nickname')
+//         res.body.trainer.should.have.property('gender')
+//         res.body.trainer.should.have.property('createdAt')
+//         res.body.trainer.should.have.property('updatedAt')
+//         res.body.trainer.should.have.property('__v')
+//         res.body.trainer.nickname.should.equal('Brock')
+//         res.should.have.status(201)
+//         done()
+//       })
+//     })
+//   })
+// })
 
-describe('trainer DELETE', function () {
-  let id = ''
-  let id_pokemon = ''
-  beforeEach(function (done) {
-    const pokemon = new Pokemon({
-      name: "Charmender",
-      level: 1,
-      type: "fire",
-    })
-    pokemon.save((err,res) => {
-      id_pokemon = res._id
-      done()
-    })
-  })
-  beforeEach(function (done) {
-    const newTrainer = new Trainer({
-      nickname: "Ash Ketchum",
-      gender: "Male",
-      pokemons: [id_pokemon]
-    })
-    newTrainer.save((err,res) => {
-      id = res._id
-      done()
-    })
-  })
+// describe('trainer DELETE', function () {
+//   let id = ''
+//   let id_pokemon = ''
+//   beforeEach(function (done) {
+//     const pokemon = new Pokemon({
+//       name: "Charmender",
+//       level: 1,
+//       type: "fire",
+//     })
+//     pokemon.save((err,res) => {
+//       id_pokemon = res._id
+//       done()
+//     })
+//   })
+//   beforeEach(function (done) {
+//     const newTrainer = new Trainer({
+//       nickname: "Ash Ketchum",
+//       gender: "Male",
+//       pokemons: [id_pokemon]
+//     })
+//     newTrainer.save((err,res) => {
+//       id = res._id
+//       done()
+//     })
+//   })
 
-  afterEach(function (done) {
-    Trainer.remove({}, function (err) {
-      done()
-    })
-  })
+//   afterEach(function (done) {
+//     Trainer.remove({}, function (err) {
+//       done()
+//     })
+//   })
 
-  afterEach(function (done) {
-    Pokemon.remove({}, function (err) {
-      done()
-    })
-  })
+//   afterEach(function (done) {
+//     Pokemon.remove({}, function (err) {
+//       done()
+//     })
+//   })
 
-  describe('delete one user', function () {
-    it('it should delete one user data ', function (done) {
-      chai.request(server)
-      .get('/api/user')
-      .end(function(err, res) {
-        res.body.should.be.an('object').to.have.property('msg')
-        res.body.should.be.an('object').to.have.property('user').with.lengthOf(1).should.be.an('object')
-        res.body.user[0].should.have.property('pokemons').with.lengthOf(1)
-        res.body.user[0].should.have.property('nickname')
-        res.body.user[0].should.have.property('gender')
-        res.body.user[0].should.have.property('createdAt')
-        res.body.user[0].should.have.property('updatedAt')
-        res.body.user[0].should.have.property('__v')
-        res.body.user[0].pokemons[0].should.have.property('_id')
-        res.body.user[0].pokemons[0].should.have.property('name')
-        res.body.user[0].pokemons[0].should.have.property('level')
-        res.body.user[0].pokemons[0].should.have.property('type')
-        res.should.have.status(200)
-        chai.request(server)
-        .delete('/api/user/'+res.body.user[0]._id)
-        .end(function(err, response){
-          response.should.have.status(201);
-          response.should.not.have.status(404);
-          response.body.should.be.a('object');
-          done();
-        })
-      })
-    })
-  })
-})
+//   describe('delete one user', function () {
+//     it('it should delete one user data ', function (done) {
+//       chai.request(server)
+//       .get('/api/user')
+//       .end(function(err, res) {
+//         res.body.should.be.an('object').to.have.property('msg')
+//         res.body.should.be.an('object').to.have.property('user').with.lengthOf(1).should.be.an('object')
+//         res.body.user[0].should.have.property('pokemons').with.lengthOf(1)
+//         res.body.user[0].should.have.property('nickname')
+//         res.body.user[0].should.have.property('gender')
+//         res.body.user[0].should.have.property('createdAt')
+//         res.body.user[0].should.have.property('updatedAt')
+//         res.body.user[0].should.have.property('__v')
+//         res.body.user[0].pokemons[0].should.have.property('_id')
+//         res.body.user[0].pokemons[0].should.have.property('name')
+//         res.body.user[0].pokemons[0].should.have.property('level')
+//         res.body.user[0].pokemons[0].should.have.property('type')
+//         res.should.have.status(200)
+//         chai.request(server)
+//         .delete('/api/user/'+res.body.user[0]._id)
+//         .end(function(err, response){
+//           response.should.have.status(201);
+//           response.should.not.have.status(404);
+//           response.body.should.be.a('object');
+//           done();
+//         })
+//       })
+//     })
+//   })
+// })
