44c4702fab0bf8dbc0e313d7511e0f1ee05ff35b
khodhirob <odirobbani@gmail.com>
2018-10-17 11:10:01 +0700

    testing user get

---
+++ controllers/user.js
@@ -0,0 +1,19 @@
+const User = require('../models/user')
+
+module.exports = {
+    getUser : (req,res) => {
+        
+    }
+    // createOne: (req, res) => {
+    //     let objExpense = {
+    //         price: req.body.price,
+    //         type: req.body.type,
+    //         description: req.body.description,
+    //         user: req.decoded._id
+    //     }
+    //     let expense = new User(objExpense)
+    //     expense.save()
+    //     .then( result => res.status(201).json({result}))
+    //     .catch( err => res.status(500).json({err}))
+    // }
+}
