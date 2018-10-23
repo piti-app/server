0612ec0c9264438e6743783abe703357e376b2e4
talkasrul <talkasrul@gmail.com>
2018-10-23 14:55:02 +0700

    Date Helper

--- helper/date.js
+++ helper/date.js
@@ -1,16 +1,16 @@
-function setDate (params){
-    let date ;
-    if(params){
-         date = new Date(params)
-    }
-    else {
-         date = new Date()
-    }
-    let dd = date.getDate()
-    let mm = date.getMonth()
-    let yy = date.getFullYear()
-    let today =  `${dd}-${mm}-${yy}`
-    return today
-}
+// function setDate (params){
+//     let date ;
+//     if(params){
+//          date = new Date(params)
+//     }
+//     else {
+//          date = new Date()
+//     }
+//     let dd = date.getDate()
+//     let mm = date.getMonth()
+//     let yy = date.getFullYear()
+//     let today =  `${dd}-${mm}-${yy}`
+//     return today
+// }
 
-module.exports = setDate
+// module.exports = setDate
